dfb6de66b2524f7e8cf5dc3685313d05c2b7a309
pussinboots <pussinboots666@googlemail.com>
2014-05-30 15:14:27 +0200

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var connect = require('connect');
-var serveStatic = require('serve-static');
 var requestCl = require("request");
 var fs = require('fs');
 var url = require('url');
@@ -8,8 +7,7 @@ var port = Number(process.env.PORT || 9000);
 var server = connect()
 server.use(function (req, res) {
 	var app = url.parse(req.url, true).query.app;
-	console.log(app);
-	requestCl.get({url:"https://"+app+".herokuapp.com/images/heroku-bdage.png",  encoding: 'binary'}, function(error, response, body) {
+	requestCl.get({url:"https://"+app+".herokuapp.com/images/heroku-badge.png",  encoding: 'binary'}, function(error, response, body) {
 		if(error || response.statusCode!=200) {
 			var errorMessage = error || response.statusCode;
 			console.log('server error image for ' + app + ' error ' + errorMessage);
