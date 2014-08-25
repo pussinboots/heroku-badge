12b01c1550e1636a8154dc971cc4e3088826508f
welcomattic <mathieu.santostefano@gmail.com>
2014-08-25 22:12:35 +0200

    add 407 code for private apps + uniformize indentations

--- server.js
+++ server.js
@@ -6,25 +6,32 @@ var url = require('url');
 
 var port = Number(process.env.PORT || 9000);
 var server = connect()
-server.use(serveStatic(__dirname+'/public'));
-server.use(function (req, res, next) {
-	var app = url.parse(req.url, true).query.app;
-	var root = url.parse(req.url, true).query.root || '';
-	requestCl.get({url:"https://"+app+".herokuapp.com/" + root}, function(error, response, body) {
-		if(error || response.statusCode!=200) {
-			var errorMessage = error || response.statusCode;
-			console.log('server error image for ' + app + ' error ' + errorMessage);
-			var fileStream = fs.createReadStream('public/images/heroku-failed.png');
-			res.writeHead(200, {'Content-Type': 'image/png', "Cache-Control:" : "no-cache, no-store, must-revalidate" });
-        		fileStream.pipe(res);
-			
-		} else {
-			console.log('server success image for ' + app);
-			var fileStream = fs.createReadStream('public/images/heroku-badge.png');
-			res.writeHead(200, {'Content-Type': 'image/png', "Cache-Control:" : "no-cache, no-store, must-revalidate" });
-        		fileStream.pipe(res);
-		}
-	});
+server.use(serveStatic(__dirname + '/public'));
+server.use(function(req, res, next) {
+  var app = url.parse(req.url, true).query.app;
+  var root = url.parse(req.url, true).query.root || '';
+  requestCl.get({
+    url: "https://" + app + ".herokuapp.com/" + root
+  }, function(error, response, body) {
+    if (error || response.statusCode != 200 || response.statusCode != 407) {
+      var errorMessage = error || response.statusCode;
+      console.log('server error image for ' + app + ' error ' + errorMessage);
+      var fileStream = fs.createReadStream('public/images/heroku-failed.png');
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        "Cache-Control:": "no-cache, no-store, must-revalidate"
+      });
+      fileStream.pipe(res);
+    } else {
+      console.log('server success image for ' + app);
+      var fileStream = fs.createReadStream('public/images/heroku-badge.png');
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        "Cache-Control:": "no-cache, no-store, must-revalidate"
+      });
+      fileStream.pipe(res);
+    }
+  });
 });
 
 server.listen(port);
