052fd10bd4adf30f5929198e98d44f25fd53b06d
Frank <pussinboots666@googlemail.com>
2015-02-17 11:53:44 +0100

    Add new query parameter style
    Add new query parameter style to support different styles maybe in the future. At the moment normal style which is the default and flat style will be supported.

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ server.use(serveStatic(__dirname + '/public'));
 server.use(function(req, res, next) {
   var app = url.parse(req.url, true).query.app;
   var root = url.parse(req.url, true).query.root || '';
+  var style = url.parse(req.url, true).query.style || '';
   requestCl.get({
     url: "https://" + app + ".herokuapp.com/" + root
   }, function(error, response, body) {
     if (error || [200,401].indexOf(response.statusCode) < 0) {
       var errorMessage = error || response.statusCode;
       console.log('server error image for ' + app + ' error ' + errorMessage);
-      var fileStream = fs.createReadStream('public/images/heroku-failed.png');
+      var fileStream = fs.createReadStream('public/images/heroku-failed' + style + '.png');
       res.writeHead(200, {
         'Content-Type': 'image/png',
         "Cache-Control:": "no-cache, no-store, must-revalidate"
@@ -24,7 +25,7 @@ server.use(function(req, res, next) {
       fileStream.pipe(res);
     } else {
       console.log('server success image for ' + app);
-      var fileStream = fs.createReadStream('public/images/heroku-badge.png');
+      var fileStream = fs.createReadStream('public/images/heroku-badge' + style + '.png');
       res.writeHead(200, {
         'Content-Type': 'image/png',
         "Cache-Control:": "no-cache, no-store, must-revalidate"
