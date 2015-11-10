63c941f0c19228a87cad19be0165c74c4fc41aaf
Frank <pussinboots666@googlemail.com>
2015-11-10 14:35:41 +0100

    First support svg approach #6

--- server.js
+++ server.js
@@ -11,27 +11,34 @@ server.use(function(req, res, next) {
   var app = url.parse(req.url, true).query.app;
   var root = url.parse(req.url, true).query.root || '';
   var style = url.parse(req.url, true).query.style || '';
+  var svg = url.parse(req.url, true).query.svg || 0;
+  var fileExtension = 'png';
+  if (svg == 1) {
+  	fileExtension = 'svg';
+  }
   if (style.length > 0) {
     if (style == 'flat') style = '-' +style;
 	else style = '';
   }
+  
   requestCl.get({
     url: "https://" + app + ".herokuapp.com/" + root
   }, function(error, response, body) {
     if (error || [200,401].indexOf(response.statusCode) < 0) {
       var errorMessage = error || response.statusCode;
       console.log('server error image for ' + app + ' error ' + errorMessage);
-      var fileStream = fs.createReadStream('public/images/heroku-failed' + style + '.png');
+      
+      var fileStream = fs.createReadStream('public/images/heroku-failed' + style + '.' + fileExtension);
       res.writeHead(200, {
-        'Content-Type': 'image/png',
+        'Content-Type': 'image/'+fileExtension,
         "Cache-Control:": "no-cache, no-store, must-revalidate"
       });
       fileStream.pipe(res);
     } else {
       console.log('server success image for ' + app);
-      var fileStream = fs.createReadStream('public/images/heroku-badge' + style + '.png');
+      var fileStream = fs.createReadStream('public/images/heroku-badge' + style + '.' + fileExtension);
       res.writeHead(200, {
-        'Content-Type': 'image/png',
+        'Content-Type': 'image/'+fileExtension,
         "Cache-Control:": "no-cache, no-store, must-revalidate"
       });
       fileStream.pipe(res);
